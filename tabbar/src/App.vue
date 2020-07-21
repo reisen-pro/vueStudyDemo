b24e2ca8efefea1959dbdb9e49151778a2de9d57
nodecode <18605103362@163.com>
2020-07-22 02:52:41 +0800

    更新学习进度

---
+++ tabbar/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <tab-bar></tab-bar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import TabBar from "./components/tabbar/TabBar";
+  export default {
+    name: 'App',
+    components: {TabBar}
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
