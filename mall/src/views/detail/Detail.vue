5b0db2a0646013ac9f412ca4ed0c6c70b1fdd88e
reisen-pro <1054004534@qq.com>
2020-08-17 01:00:07 +0800

    商品信息参数的展示

--- mall/src/views/detail/Detail.vue
+++ mall/src/views/detail/Detail.vue
@@ -5,6 +5,7 @@
       <detail-swiper :top-images="topImages"/>
       <detail-base-info :goods-info="goodsInfo"/>
       <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
     </scroll>
   </div>
 </template>
@@ -15,6 +16,7 @@
   import DetailSwiper from "./childComps/DetailSwiper";
   import DetailBaseInfo from "./childComps/DetailBaseInfo";
   import DetailShopInfo from "./childComps/DetailShopInfo";
+  import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
 
   import {getDetail, GoodsInfo, Shop} from "network/detail";
 
@@ -27,7 +29,8 @@
       DetailNavBar,
       DetailSwiper,
       DetailShopInfo,
-      Scroll
+      DetailGoodsInfo,
+      Scroll,
     },
     data() {
       return {
@@ -35,7 +38,8 @@
         topImages: [],
         goodsInfo: {},
         shop: {},
-        detailImages:{}
+        detailImages: {},
+        detailInfo: {}
       }
     },
     created() {
@@ -44,14 +48,27 @@
 
       // 根据id请求详情数据
       getDetail(this.id).then(res => {
-
         const data = res.data;
+
+        console.log(data)
+
+        // 获取顶部轮播图片数据
         this.topImages = data.topImages;
 
+        // 将商品信息封装到GoodsInfo类中
         this.goodsInfo = new GoodsInfo(data, data.columns, data.services);
 
+        // 将店铺信息封装到Shop类中
         this.shop = new Shop(data.shopInfo)
+
+        // 保存商品的详情数据
+        this.detailInfo = data.detailInfo;
       })
+    },
+    methods: {
+      imageLoad() {
+        this.$refs.scroll.refresh()
+      }
     }
   }
 </script>
